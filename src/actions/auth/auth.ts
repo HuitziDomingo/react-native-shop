ea7329bd6327d1a7e61925b0473e18c3ccf54730
HuitziDomingo <false>
2024-03-31 23:25:44 -0600

    acciones del auth

---
+++ src/actions/auth/auth.ts
@@ -0,0 +1,35 @@
+import { tesloApi } from "../../config/config/tesloApi"
+import { UserEntity } from "../../domain/entities/user.entity"
+import type { AuthResponse } from "../../infrastructure/interfaces/auth.response"
+
+
+const returnUserToken = (data: AuthResponse) => {
+    const user: UserEntity = {
+        id: data.id,
+        email: data.email,
+        fullName: data.fullName,
+        isActive: data.isActive,
+        roles: data.roles
+    }
+    return {
+        user,
+        token: data.token
+
+    }
+}
+
+
+
+export const authLogin = async (email: string, password: string) => {
+    email.toLowerCase()
+    try {
+        const { data } = await tesloApi.post<AuthResponse>('/auth/login', {
+            email,
+            password,
+        })
+        return returnUserToken(data)
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
